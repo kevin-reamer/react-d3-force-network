7da3d04eeb22b15e635e41d8175af0eeb4c79c89
Kevin Reamer <reamer.kevin@gmail.com>
2019-07-01 10:48:19 -0700

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DemoContainer from './containers/DemoContainer/DemoContainer';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <DemoContainer />
+    </div>
+  );
+}
+
+export default App;
